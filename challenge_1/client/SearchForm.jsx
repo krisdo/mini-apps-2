c8fcf41214ac0913c9819776deb4ef5ce07ed549
Krissy Do <krissy@Krisss-MacBook-Pro.local>
2020-08-06 19:31:18 -0700

    added search form

---
+++ challenge_1/client/SearchForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class SearchForm extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          keyword: null
+        }
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+      this.setState({
+        keyword: event.target.value
+      })
+    }
+
+    handleSearch(event){
+      const { keyword } = this.state;
+      this.props.search(keyword);
+      event.preventDefault();
+    }
+    
+    render() {
+        return(
+            <form onSubmit={this.handleSearch}>
+                <label>
+                  Search
+                  <input type="text" placeholder='keyword' value={this.state.value}  onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+
+export default SearchForm;
--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import SearchForm from './SearchForm.jsx'
 
 class App extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            data: null
+        }
+        this.search.bind(this);
+    }
 
+    search(keyword) {
+        
+        axios.get(`/events?q=${keyword}`)
+        .then( ({data}) => {
+            console.log(data);
+            this.setState({data});
+        })
+        .catch( (error) => {
+            //TODO: handle error?
+            console.log(error);
+        })
+    }
+
+    componentDidMount(){
+
+    }
+    
     render() {
         return(
             <div>
-                Historical Events Finder
+                
+                    <h1>Historical Events Finder</h1>
+              
+                    <SearchForm search={this.search}/>
+                
             </div>
         )
     }
